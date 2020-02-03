4b07c369358eada29adfefde0b8efeffccfe9735
Petrickvu <57941956+Petrickvu@users.noreply.github.com>
2020-02-03 10:05:28 +0100

    Script test 2

--- J.E.P Periode 3/Assets/Test.cs
+++ J.E.P Periode 3/Assets/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //test 1
+        //test 2
     }
 
     // Update is called once per frame
