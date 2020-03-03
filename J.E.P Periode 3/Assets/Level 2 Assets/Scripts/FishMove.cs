b35e440dfc46725e2a1a209ccc2ba4f3e0179046
Jasper0076 <60340192+Jasper0076@users.noreply.github.com>
2020-03-03 16:29:28 +0100

    Bubble particle.AnimationController

--- J.E.P Periode 3/Assets/Level 2 Assets/Scripts/FishMove.cs
+++ J.E.P Periode 3/Assets/Level 2 Assets/Scripts/FishMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FishMove : MonoBehaviour
 {
-  public float velocidadMax;
+  public float maxVelocity;
      
      public float xMax;
      public float zMax;
@@ -13,56 +13,56 @@ public class FishMove : MonoBehaviour
          
      private float x;
      private float z;
-     private float tiempo;
-     private float angulo;
+     private float speed;
+     private float angle;
  
      // Use this for initialization
      void Start () {
  
  
-         x = Random.Range(-velocidadMax, velocidadMax);
-         z = Random.Range(-velocidadMax, velocidadMax);
-         angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
-         transform.localRotation = Quaternion.Euler( 0, angulo, 0);
+         x = Random.Range(-maxVelocity, maxVelocity);
+         z = Random.Range(-maxVelocity, maxVelocity);
+         angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
+         transform.localRotation = Quaternion.Euler( 0, angle, 0);
      }
      //lerp 
      // Update is called once per frame
      void Update () {
  
-         tiempo += Time.deltaTime;
+         speed += Time.deltaTime;
  
          if (transform.localPosition.x > xMax) {
-             x = Random.Range(-velocidadMax, 0.0f);
-             angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
-             transform.localRotation = Quaternion.Euler(0, angulo, 0);
-             tiempo = 0.0f; 
+             x = Random.Range(-maxVelocity, 0.0f);
+             angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
+             transform.localRotation = Quaternion.Euler(0, angle, 0);
+             speed = 0.0f; 
          }
          if (transform.localPosition.x < xMin) {
-             x = Random.Range(0.0f, velocidadMax);
-             angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
-             transform.localRotation = Quaternion.Euler(0, angulo, 0); 
-             tiempo = 0.0f; 
+             x = Random.Range(0.0f, maxVelocity);
+             angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
+             transform.localRotation = Quaternion.Euler(0, angle, 0); 
+             speed = 0.0f; 
          }
          if (transform.localPosition.z > zMax) {
-             z = Random.Range(-velocidadMax, 0.0f);
-             angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
-             transform.localRotation = Quaternion.Euler(0, angulo, 0); 
-             tiempo = 0.0f; 
+             z = Random.Range(-maxVelocity, 0.0f);
+             angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
+             transform.localRotation = Quaternion.Euler(0, angle, 0); 
+             speed = 0.0f; 
          }
          if (transform.localPosition.z < zMin) {
-             z = Random.Range(0.0f, velocidadMax);
-             angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
-             transform.localRotation = Quaternion.Euler(0, angulo, 0);
-             tiempo = 0.0f; 
+             z = Random.Range(0.0f, maxVelocity);
+             angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
+             transform.localRotation = Quaternion.Euler(0, angle, 0);
+             speed = 0.0f; 
          }
  
  
-         if (tiempo > 1.0f) {
-             x = Random.Range(-velocidadMax, velocidadMax);
-             z = Random.Range(-velocidadMax, velocidadMax);
-             angulo = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
-             transform.localRotation = Quaternion.Euler(0, angulo, 0);
-             tiempo = 0.0f;
+         if (speed > 1.0f) {
+             x = Random.Range(-maxVelocity, maxVelocity);
+             z = Random.Range(-maxVelocity, maxVelocity);
+             angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
+             transform.localRotation = Quaternion.Euler(0, angle, 0);
+             speed = 0.0f;
          }
  
          transform.localPosition = new Vector3(transform.localPosition.x + x, transform.localPosition.y, transform.localPosition.z + z);
