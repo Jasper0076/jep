602a2604bdf55a1c153a16766747802edd0c6fee
Jasper0076 <60340192+Jasper0076@users.noreply.github.com>
2020-03-04 14:02:50 +0100

    MoveFish

--- J.E.P Periode 3/Assets/Level 2 Assets/Scripts/FishMove.cs
+++ J.E.P Periode 3/Assets/Level 2 Assets/Scripts/FishMove.cs
@@ -4,67 +4,31 @@ using UnityEngine;
 
 public class FishMove : MonoBehaviour
 {
-  public float maxVelocity;
-     
-     public float xMax;
-     public float zMax;
-     public float xMin;
-     public float zMin;
-         
-     private float x;
-     private float z;
-     private float speed;
-     private float angle;
- 
-     // Use this for initialization
-     void Start () {
- 
- 
-         x = Random.Range(-maxVelocity, maxVelocity);
-         z = Random.Range(-maxVelocity, maxVelocity);
-         angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
-         transform.localRotation = Quaternion.Euler( 0, angle, 0);
-     }
-     //lerp 
-     // Update is called once per frame
-     void Update () {
- 
-         speed += Time.deltaTime;
- 
-         if (transform.localPosition.x > xMax) {
-             x = Random.Range(-maxVelocity, 0.0f);
-             angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
-             transform.localRotation = Quaternion.Euler(0, angle, 0);
-             speed = 0.0f; 
-         }
-         if (transform.localPosition.x < xMin) {
-             x = Random.Range(0.0f, maxVelocity);
-             angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
-             transform.localRotation = Quaternion.Euler(0, angle, 0); 
-             speed = 0.0f; 
-         }
-         if (transform.localPosition.z > zMax) {
-             z = Random.Range(-maxVelocity, 0.0f);
-             angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
-             transform.localRotation = Quaternion.Euler(0, angle, 0); 
-             speed = 0.0f; 
-         }
-         if (transform.localPosition.z < zMin) {
-             z = Random.Range(0.0f, maxVelocity);
-             angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
-             transform.localRotation = Quaternion.Euler(0, angle, 0);
-             speed = 0.0f; 
-         }
- 
- 
-         if (speed > 1.0f) {
-             x = Random.Range(-maxVelocity, maxVelocity);
-             z = Random.Range(-maxVelocity, maxVelocity);
-             angle = Mathf.Atan2(x, z) * (180 / 3.141592f) + 180;
-             transform.localRotation = Quaternion.Euler(0, angle, 0);
-             speed = 0.0f;
-         }
- 
-         transform.localPosition = new Vector3(transform.localPosition.x + x, transform.localPosition.y, transform.localPosition.z + z);
-     }
+    public float maxVelocity;
+
+
+    public Vector2 xMinMax;
+    public Vector2 zMinMax;
+    public float moveSpeed;
+    public Vector3 targetPos;
+    private float targetReachedDistance = 0.1f;
+
+    //slerp  from toRotation 
+    // Update is called once per frame
+    void Update() {
+        if(Vector3.Distance(transform.position, targetPos) < targetReachedDistance)
+        {
+            targetPos = GetRandomPos();
+        }
+
+        // Hier even lerpen, en klaar.
+        // Lerpen van cur rot, naar de richting van targetPos.
+
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+    }
+
+    Vector3 GetRandomPos()
+    {
+        return new Vector3(Random.Range(xMinMax.x, xMinMax.y), 0, Random.Range(zMinMax.x, zMinMax.y));
+    }
 }
