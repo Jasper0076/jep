72b1d85f8d281d3c4872af8d5764e0dbf06b024f
Jasper0076 <60340192+Jasper0076@users.noreply.github.com>
2020-03-24 16:23:48 +0100

    BuildingMatSwitch

---
+++ J.E.P Periode 3/Assets/OverWorld Assets/MatSwitch.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatSwitch : MonoBehaviour
+{
+
+    public Material redMat;
+    public Material purpMat;
+    public Material yellowMat;
+    public Material blueMat;
+    public MeshRenderer meshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToPurple()
+    {
+        meshRenderer.material = purpMat;
+    }
+
+    public void ToBlue()
+    {
+        meshRenderer.material = blueMat;
+    }
+
+    public void ToRed()
+    {
+        meshRenderer.material = redMat;
+    }
+
+    public void ToYellow()
+    {
+        meshRenderer.material = yellowMat;
+    }
+
+    
+}
+
