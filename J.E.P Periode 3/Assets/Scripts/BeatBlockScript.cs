ba80464fdfefa23f11b6d75a490b9b000b1ba8bb
Jasper0076 <60340192+Jasper0076@users.noreply.github.com>
2020-03-24 15:39:18 +0100

    Revert "buttons"
    This reverts commit 519cfb88587b0882a6296674cdc19217c89a581a.

--- J.E.P Periode 3/Assets/Scripts/BeatBlockScript.cs
+++ J.E.P Periode 3/Assets/Scripts/BeatBlockScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class BeatBlockScript : MonoBehaviour
 {
+    public float bpm;
     public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        speed = speed * -1;
+        speed = bpm / -60f * Time.deltaTime;
     }
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, speed * Time.deltaTime);
+        transform.Translate(0, 0, speed);
+    }
+
+    public IEnumerator TestWait()
+    {
+        yield return new WaitForSeconds(1);
+        print("Ik heb een seconden gewacht!");
     }
 }
